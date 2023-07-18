9df00ada9e16eb9e5645eaaea74081bd71f565a3
Tomáš Vojík <vojik@wboy.cz>
2023-07-18 17:06:11 +0200

    :sparkles: Extended the GameDataExtensionInterface for Player and Team models

--- src/GameDataExtensionInterface.php
+++ src/GameDataExtensionInterface.php
@@ -3,25 +3,31 @@
 namespace LAC\Modules\Core;
 
 use App\GameModels\Game\Game;
+use App\GameModels\Game\Player;
+use App\GameModels\Game\Team;
 use Dibi\Row;
 
 interface GameDataExtensionInterface
 {
 
-	public function parseRow(Row $row, Game $game): void;
+	public function init(Game|Team|Player $game): void;
+
+	public function parseRow(Row $row, Game|Team|Player $game): void;
 
 	/**
 	 * @param array<string,mixed> $data
 	 * @param Game $game
 	 * @return void
 	 */
-	public function addQueryData(array &$data, Game $game): void;
+	public function addQueryData(array &$data, Game|Team|Player $game): void;
 
 	/**
 	 * @param array<string,mixed> $data
 	 * @param Game $game
 	 * @return void
 	 */
-	public function addJsonData(array &$data, Game $game): void;
+	public function addJsonData(array &$data, Game|Team|Player $game): void;
+
+	public function save(Game|Team|Player $game): bool;
 
 }
