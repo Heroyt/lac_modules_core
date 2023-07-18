17c3beb3d66d5ade48ba2ee3148c353e9d6e8fea
Tomáš Vojík <vojik@wboy.cz>
2023-07-18 17:04:55 +0200

    :sparkles: Added LigaApi module extension

---
+++ src/LigaApiExtensionInterface.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace LAC\Modules\Core;
+
+use App\GameModels\Game\Game;
+
+interface LigaApiExtensionInterface
+{
+
+	/**
+	 * @param string $system
+	 * @param Game[] $games
+	 * @return void
+	 */
+	public function beforeGameSync(string $system, array $games): void;
+
+	public function processGameBeforeSync(Game $game): void;
+
+	/**
+	 * @param string $system
+	 * @param Game[] $games
+	 * @return void
+	 */
+	public function afterGameSync(string $system, array $games): void;
+
+}
